67087b3d158fdfa3012e05818c1fbb9604dffcba
Gabriel Filion <lelutin@gmail.com>
2011-02-05 17:17:47 -0500

    options.py: update docstrings and detail optspec
    The docstring on the Options class currently refers to a man page which
    does not exist, and still talks about the now-removed 'exe' parameter.
    Update this to be more accurate.
    
    Add a docstring to OptDict.
    
    Finally, the options.py file brings a concept of option spec string. Its
    construction should be documented. Since we'd like the options.py file
    to be a one-file drop-in so that it can be easily used in other
    projects, let's document the option specs in the module's docstring.
    
    Signed-off-by: Gabriel Filion <lelutin@gmail.com>

--- lib/bup/options.py
+++ lib/bup/options.py
@@ -1,9 +1,42 @@
 """Command-line options parser.
 With the help of an options spec string, easily parse command-line options.
+
+An options spec is made up of two parts, separated by a line with two dashes.
+The first part is the synopsis of the command and the second one specifies
+options, one per line.
+
+Each non-empty line in the synopsis gives a set of options that can be used
+together.
+
+Option flags must be at the begining of the line and multiple flags are
+separated by commas. Usually, options have a short, one character flag, and a
+longer one, but the short one can be omitted.
+
+Long option flags are used as the option's key for the OptDict produced when
+parsing options.
+
+When the flag definition is ended with an equal sign, the option takes one
+string as an argument. Otherwise, the option does not take an argument and
+corresponds to a boolean flag that is true when the option is given on the
+command line.
+
+The option's description is found at the right of its flags definition, after
+one or more spaces. The description ends at the end of the line. If the
+description contains text enclosed in square brackets, the enclosed text will
+be used as the option's default value.
+
+Options can be put in different groups. Options in the same group must be on
+consecutive lines. Groups are formed by inserting a line that begins with a
+space. The text on that line will be output after an empty line.
 """
 import sys, os, textwrap, getopt, re, struct
 
 class OptDict:
+    """Dictionary that exposes keys as attributes.
+
+    Keys can bet set or accessed with a "no-" or "no_" prefix to negate the
+    value.
+    """
     def __init__(self):
         self._opts = {}
 
@@ -65,10 +98,9 @@ def _tty_width():
 
 class Options:
     """Option parser.
-    When constructed, two strings are mandatory. The first one is the command
-    name showed before error messages. The second one is a string called an
-    optspec that specifies the synopsis and option flags and their description.
-    For more information about optspecs, consult the bup-options(1) man page.
+    When constructed, a string called an option spec must be given. It
+    specifies the synopsis and option flags and their description.  For more
+    information about option specs, see the docstring at the top of this file.
 
     Two optional arguments specify an alternative parsing function and an
     alternative behaviour on abort (after having output the usage string).
