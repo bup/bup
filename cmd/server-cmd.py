9c4c2dd9f1934658346212f5671df84c1dcb6a5e
Avery Pennarun <apenwarr@gmail.com>
2011-02-17 04:54:13 -0800

    cmd/server: add a debug message saying which object caused a suggestion.
    Let's use this to try to debug Aneurin's problem (and potentially others).
    
    Signed-off-by: Avery Pennarun <apenwarr@gmail.com>

--- cmd/server-cmd.py
+++ cmd/server-cmd.py
@@ -102,6 +102,8 @@ def receive_objects_v2(conn, junk):
                 (dir,name) = os.path.split(oldpack)
                 if not (name in suggested):
                     debug1("bup server: suggesting index %s\n" % name)
+                    debug1("bup server:   because of object %s\n"
+                           % shar.encode('hex'))
                     conn.write('index %s\n' % name)
                     suggested.add(name)
                 continue
