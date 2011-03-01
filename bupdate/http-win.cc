bddef96be1e74f9c4adb2ccb507b4d93e79af643
Avery Pennarun <apenwarr@gmail.com>
2011-03-01 01:54:21 -0800

    http-win: now gets a valid list of files.

--- bupdate/http-win.cc
+++ bupdate/http-win.cc
@@ -1,12 +1,29 @@
 #include "wvcom.h"
 #include "wvcomstatus.h"
 #include "wvbuf.h"
+#include "wvstringlist.h"
+#include "wvstrutils.h"
+#include "wvdiriter.h"
+#include <unistd.h>
+
+
+typedef void progfunc_t(const char *s);
+typedef unsigned char  byte;
+
+static void simple_print(const char *s)
+{
+    printf("%s", s);
+    fflush(stdout);
+}
+
+
+// reassign this to change progress message printing function
+progfunc_t *progfunc = simple_print;
 
 
 void print(WvStringParm s)
 {
-    printf("%s", s.cstr());
-    fflush(stdout);
+    simple_print(s);
 }
 
 
@@ -58,7 +75,7 @@ WvError http_get(WvBuf &buf, WvStringParm url, int startbyte, int bytelen)
 			    status, expected);
     }
 
-    unsigned char *b = NULL;
+    byte *b = NULL;
     size_t len = 0;
     WvVariant v = req.get("responseBody");
     v.blob(&b, &len);
@@ -75,18 +92,166 @@ WvError http_get(WvBuf &buf, WvStringParm url, int startbyte, int bytelen)
 }
 
 
-int main()
+WvString http_get_str(WvStringParm url)
 {
+    WvComStatus err;
+    WvDynBuf b;
+    err.set(http_get(b, url, 0, -1));
+    if (err.isok())
+	return b.getstr();
+    else
+	return WvString::null;
+}
+
+
+bool is_url(WvStringParm s)
+{
+    return strstr(s, "://");
+}
+
+
+void targets_from_file(WvStringList &l, WvStringParm s)
+{
+    if (s.startswith("<"))
+    {
+	// it's HTML; pick out the anchors
+	const char *cptr = s;
+	while (cptr)
+	{
+	    cptr = strstr(cptr, "<a href=");
+	    if (!cptr)
+		break;
+	    cptr += 8;
+	    char quote = *cptr;
+	    if (quote != '/' && quote != '\"')
+		continue;
+	    cptr++;
+	    const char *eptr = strchr(cptr, quote);
+	    if (!eptr)
+		continue;
+	    WvString ns;
+	    size_t len = eptr-cptr;
+	    ns.setsize(len+1);
+	    char *optr = ns.edit();
+	    strncpy(optr, cptr, len);
+	    optr[len] = 0;
+	    l.append(url_decode(ns));
+	}
+    }
+    else
+    {
+	// it's not HTML; assume it's a one-per-line list of filenames
+	l.split(s, "\n");
+    }
+}
+
+
+WvString readfile(WvStringParm filename)
+{
+    byte buf[65536];
+    WvDynBuf b;
+    FILE *f = fopen(filename, "rb");
+    if (!f)
+	return WvString::null;
+    size_t len;
+    while ((len = fread(buf, 1, sizeof(buf), f)) >= 1)
+	b.put(buf, len);
+    fclose(f);
+    return b.getstr();
+}
+
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+	fprintf(stderr, "usage: %s <url>\n", argv[0]);
+	return 1;
+    }
+    
+    WvComStatus err;
+    WvString baseurl(argv[1]);
+    for (char *cptr = baseurl.edit(); cptr && *cptr; cptr++)
+	if (*cptr == '\\')
+	    *cptr = '/';
+    
+    WvStringList targets;
+    if (baseurl.endswith(".fidx"))
+    {
+	// the baseurl is a particular fidx, not a file list, so just use
+	// a file list of one.
+	targets.append(getfilename(baseurl));
+    }
+    else if (is_url(baseurl))
+    {
+	// it's an actual URL; download it
+	print("Downloading base: %s\n", baseurl);
+	WvString s = http_get_str(baseurl);
+	// FIXME: what if we got http redirected?  We should save the new url
+	targets_from_file(targets, s);
+    }
+    else
+    {
+	// not an URL, so it's a disk file
+	struct stat st;
+	while (baseurl.endswith("/"))
+	    *strrchr(baseurl.edit(), '/') = 0;
+	if (stat(baseurl, &st) != 0)
+	    err.set(errno);
+	else if (S_ISDIR(st.st_mode))
+	{
+	    // a directory
+	    print("it's a dir\n");
+	    WvDirIter di(baseurl);
+	    for (di.rewind(); di.next(); )
+		targets.append(di->name);
+	    if (!baseurl.endswith("/"))
+		baseurl.append("/");
+	}
+	else
+	{
+	    // an index file
+	    print("it's a file\n");
+	    targets_from_file(targets, readfile(baseurl));
+	}
+	baseurl = WvString("file://%s", baseurl);
+    }
+    if (!baseurl.endswith("/"))
+    {
+	baseurl = getdirname(baseurl);
+	if (!baseurl.endswith("/"))
+	    baseurl.append("/");
+    }
+    
+    {
+	WvStringList::Iter i(targets);
+	for (i.rewind(); i.next(); )
+	{
+	    *i = trim_string(i->edit());
+	    if (!*i || i->startswith(".") || !i->endswith(".fidx"))
+		i.xunlink();
+	}
+    }
+    
+    print("baseurl is: '%s'\n"
+	  "Targets (%s):\n",
+	  baseurl, targets.count());
+    WvStringList::Iter i(targets);
+    for (i.rewind(); i.next(); )
+	print("  '%s'\n", *i);
+#if 0    
     for (int i = 0; i < 10; i++)
     {
 	WvDynBuf buf;
-	WvError e = http_get(buf,
-		       "http://afterlife/~apenwarr/music/01 Volcanic Jig.m4a",
-			     i, 10*i);
+	WvError e = http_get(buf, baseurl, i, 10*i);
 	if (!e.isok())
 	    print("  ERROR: %s\n", e.str());
 	else
 	    print("  got %s bytes\n", buf.used());
     }
+#endif
+    
+    if (!err.isok())
+	print("error was: %s\n", err.str());
     return 0;
 }
