51bd5832b391f4c128cc42a3a5970733775f1ba7
Avery Pennarun <apenwarr@gmail.com>
2011-05-29 20:31:43 -0400

    t/test.sh: 'ls' on NetBSD sets -A by default as root; work around it.
    We have to undo it by grepping out any dotfiles.  (Normal OSes don't
    auto-set -A, but this is harmless there.)
    
    Reported by Thomas Klausner.  Fix suggested by Gabriel Filion.
    
    Signed-off-by: Avery Pennarun <apenwarr@gmail.com>

--- t/test.sh
+++ t/test.sh
@@ -446,7 +446,11 @@ mkdir $D
 WVPASS bup init
 WVPASS bup index $TOP/Documentation
 WVPASS bup save -n compression -0 --strip $TOP/Documentation
-WVPASSEQ "$(bup ls compression/latest/ | sort)" "$(ls $TOP/Documentation | sort)"
+# 'ls' on NetBSD sets -A by default when running as root, so we have to undo
+# it by grepping out any dotfiles.  (Normal OSes don't auto-set -A, but this
+# is harmless there.)
+WVPASSEQ "$(bup ls compression/latest/ | sort)" \
+	 "$(ls $TOP/Documentation | grep -v '^\.' | sort)"
 COMPRESSION_0_SIZE=$(du -s $D | cut -f1)
 
 D=compression9.tmp
