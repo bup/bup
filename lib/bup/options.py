02bd2b566ea5eec2fd656e0ae572b4c7b6b9550a
Avery Pennarun <apenwarr@gmail.com>
2012-03-19 20:48:29 -0400

    options.py: get rid of end-of-line whitespace.
    Signed-off-by: Avery Pennarun <apenwarr@gmail.com>

--- lib/bup/options.py
+++ lib/bup/options.py
@@ -3,19 +3,19 @@
 #
 # (This license applies to this file but not necessarily the other files in
 # this package.)
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-# 
+#
 #    1. Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
-# 
+#
 #    2. Redistributions in binary form must reproduce the above copyright
 #       notice, this list of conditions and the following disclaimer in
 #       the documentation and/or other materials provided with the
 #       distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY AVERY PENNARUN ``AS IS'' AND ANY
 # EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
