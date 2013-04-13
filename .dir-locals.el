5a949536f4282231779d1bb04de70aebcac02b89
Rob Browning <rlb@defaultvalue.org>
2013-04-13 17:43:33 -0500

    .dir-locals: rename python-indent (obsolete) to python-indent-offset.
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ((nil . ())
  (python-mode . ((indent-tabs-mode . nil)
-                 (python-indent . 4)))
+                 (python-indent-offset . 4)))
  (sh-mode . ((indent-tabs-mode . nil)
              (sh-basic-offset . 4)))
  (c-mode . ((indent-tabs-mode . nil)
