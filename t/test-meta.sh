da6659045814b7e7a9f002da02baa26779a21420
Thomas Klausner <wiz@NetBSD.org>
2013-11-03 12:55:27 +0100

    test-meta.sh: prefer the more portable test '=' to '=='.
    Signed-off-by: Thomas Klausner <wiz@NetBSD.org>
    [rlb@defaultvalue.org: adjust commit message.]
    Reviewed-by: Rob Browning <rlb@defaultvalue.org>

--- t/test-meta.sh
+++ t/test-meta.sh
@@ -480,7 +480,7 @@ src/foo/3"
 
 # Test ownership restoration (when root or fakeroot).
 (
-    if [ $(t/root-status) == none ]; then
+    if [ $(t/root-status) = none ]; then
         exit 0
     fi
 
@@ -591,7 +591,7 @@ src/foo/3"
 
 # Root-only tests that require an FS with all the trimmings: ACLs,
 # Linux attr, Linux xattr, etc.
-if [ $(t/root-status) == root ]; then
+if [ $(t/root-status) = root ]; then
     (
         # Some cleanup handled in universal-cleanup() above.
         # These tests are only likely to work under Linux for now
