3b254514491fc04a5bb490c0bcf045168f696fee
Rob Browning <rlb@defaultvalue.org>
2013-11-11 12:03:25 -0600

    Accommodate systems where paths inherit their group from the parent dir.
    Thanks to Thomas Klausner <tk@giga.or.at> for reporting the problem
    and helping test the solution.
    
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>

--- t/test-restore-map-owner.sh
+++ t/test-restore-map-owner.sh
@@ -31,6 +31,8 @@ WVPASS cd "$tmpdir"
 WVSTART "restore --map-user/group/uid/gid (control)"
 WVPASS mkdir src
 WVPASS touch src/foo
+# Some systems assign the parent dir group to new paths.
+WVPASS chgrp -R "$group" src
 WVPASS bup index src
 WVPASS bup save -n src src
 WVPASS bup restore -C dest "src/latest/$(pwd)/src/"
