c1953e40fb64f3a7df3631a87876d96c395ee6e4
Rob Browning <rlb@defaultvalue.org>
2014-06-02 11:36:27 -0500

    import-rdiff-backup-cmd: invoke mktemp more portably
    This should fix a problem on NetBSD 6, reported by Greg Troxel
    <gdt@lexort.com>.
    
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Reviewed-by: Greg Troxel <gdt@lexort.com>
    Tested-by: Greg Troxel <gdt@lexort.com>

--- cmd/import-rdiff-backup-cmd.sh
+++ cmd/import-rdiff-backup-cmd.sh
@@ -55,7 +55,7 @@ backups_count=$(echo "$backups" | must wc -l) || exit $?
 counter=1
 echo "$backups" |
 while read timestamp type; do
-    tmpdir=$(must mktemp -d) || exit $?
+    tmpdir=$(must mktemp -d import-rdiff-backup-XXXXXXX) || exit $?
 
     echo "Importing backup from $(date -d @$timestamp +%c) " \
         "($counter / $backups_count)" 1>&2
@@ -66,7 +66,7 @@ while read timestamp type; do
     echo 1>&2
 
     echo "Importing into bup..." 1>&2
-    TMPIDX=$(must mktemp -u) || exit $?
+    TMPIDX=$(must mktemp -u import-rdiff-backup-idx-XXXXXXX) || exit $?
     must bup index -ux -f "$tmpidx" "$tmpdir"
     must bup save --strip --date="$timestamp" -f "$tmpidx" -n "$branch" "$tmpdir"
     must rm -f "$tmpidx"
