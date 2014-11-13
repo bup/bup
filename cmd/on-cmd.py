7feb408c2d9388fd85e02372a1cbb01520beb450
Rob Browning <rlb@defaultvalue.org>
2014-11-13 12:17:23 -0600

    on: only wait for a process if it might exist
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- cmd/on-cmd.py
+++ cmd/on-cmd.py
@@ -27,32 +27,31 @@ sp = None
 p = None
 ret = 99
 
+hp = extra[0].split(':')
+if len(hp) == 1:
+    (hostname, port) = (hp[0], None)
+else:
+    (hostname, port) = hp
+
+argv = extra[1:]
+p = ssh.connect(hostname, port, 'on--server')
+
 try:
-    hp = extra[0].split(':')
-    if len(hp) == 1:
-        (hostname, port) = (hp[0], None)
-    else:
-        (hostname, port) = hp
-
-    argv = extra[1:]
-    p = ssh.connect(hostname, port, 'on--server')
-
     argvs = '\0'.join(['bup'] + argv)
     p.stdin.write(struct.pack('!I', len(argvs)) + argvs)
     p.stdin.flush()
-
     sp = subprocess.Popen([path.exe(), 'server'],
                           stdin=p.stdout, stdout=p.stdin)
     p.stdin.close()
     p.stdout.close()
-
 finally:
     while 1:
         # if we get a signal while waiting, we have to keep waiting, just
         # in case our child doesn't die.
         try:
             ret = p.wait()
-            sp.wait()
+            if sp:
+                sp.wait()
             break
         except SigException, e:
             log('\nbup on: %s\n' % e)
