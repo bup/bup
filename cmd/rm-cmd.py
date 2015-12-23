8d86a9ff7e6bd0d709e2c3b51188cd0200a1ab02
Paul Kronenwetter <kronenpj@gmail.com>
2015-12-22 21:05:33 -0500

    rm-cmd: add the bup-python #! preamble
    Signed-off-by: Paul Kronenwetter <kronenpj@gmail.com>
    [rlb@defaultvalue.org: remove everything except preamble update;
     rebase on current rm branch; adjust commit message]
    Reviewed-by: Rob Browning <rlb@defaultvalue.org>
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- cmd/rm-cmd.py
+++ cmd/rm-cmd.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python
+#!/bin/sh
+"""": # -*-python-*-
+bup_python="$(dirname "$0")/bup-python" || exit $?
+exec "$bup_python" "$0" ${1+"$@"}
+"""
+# end of bup preamble
 
 import sys
 
