f73e1dd90ccdd8b99d30daa6b246f3d4dc0df392
Rob Browning <rlb@defaultvalue.org>
2019-10-20 11:48:58 -0500

    vfs: rely on FakeLink's target in readlink
    This could result in an error like:
    
      File "/usr/local/lib/bup/bup/vfs.py", line 524, in _compute_item_size
        return len(_readlink(repo, item.oid))
      AttributeError: 'FakeLink' object has no attribute 'oid'
    
    Thanks to Hartmut Krafft for reporting the problem and helping devise
    the solution.
    
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- lib/bup/vfs.py
+++ lib/bup/vfs.py
@@ -507,12 +507,12 @@ def readlink(repo, item):
     target from the repository if necessary."""
     assert repo
     assert S_ISLNK(item_mode(item))
+    if isinstance(item, FakeLink):
+        return item.target
     if isinstance(item.meta, Metadata):
         target = item.meta.symlink_target
         if target:
             return target
-    elif isinstance(item, FakeLink):
-        return item.target
     return _readlink(repo, item.oid)
 
 def _compute_item_size(repo, item):
@@ -521,6 +521,8 @@ def _compute_item_size(repo, item):
         size = _normal_or_chunked_file_size(repo, item.oid)
         return size
     if S_ISLNK(mode):
+        if isinstance(item, FakeLink):
+            return len(item.target)
         return len(_readlink(repo, item.oid))
     return 0
 
