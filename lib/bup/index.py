2c8589b41c42361b429720fb975f48463e26711a
Johannes Berg <johannes@sipsolutions.net>
2020-01-29 00:20:09 +0100

    index: fix Entry comparison methods
    Some of these methods are evidently not used today,
    but they should have proper arguments anyway.
    
    Fixes: 3ff7455dd474 ("Don't use cmp()")
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- lib/bup/index.py
+++ lib/bup/index.py
@@ -298,7 +298,7 @@ class Entry:
     def __eq__(self, other):
         return self._cmp(other) == 0
 
-    def __ne__():
+    def __ne__(self, other):
         return self._cmp(other) != 0
 
     def __lt__(self, other):
@@ -307,10 +307,10 @@ class Entry:
     def __gt__(self, other):
         return self._cmp(other) > 0
 
-    def __le__():
+    def __le__(self, other):
         return self._cmp(other) <= 0
 
-    def __ge__():
+    def __ge__(self, other):
         return self._cmp(other) >= 0
 
     def write(self, f):
