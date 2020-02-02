bup/bup

commit 22d01e1a8077df173e03c6c7d6141fecbcdffaca

If you specified the port number on the command line, convert it to an int.
This gets rid of an exception.

Signed-off-by: Avery Pennarun <apenwarr@gmail.com>

lib/bup/client.py
@@ -78,7 +78,7 @@ class Client:
                     raise ClientError, 'connect: %s' % e, sys.exc_info()[2]
             elif self.protocol == 'bup':
                 self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.sock.connect((self.host, self.port or 1982))
+                self.sock.connect((self.host, atoi(self.port) or 1982))
                 self.sockw = self.sock.makefile('wb')
                 self.conn = DemuxConn(self.sock.fileno(), self.sockw)
         if self.dir:

commit 041082521f8bdc46e6a36347a9f5183abbe4f272

client: import atoi
Fixes: 22d01e1a8077 ("If you specified the port number on the command line, convert it to an int.")

Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
Reviewed-by: Rob Browning <rlb@defaultvalue.org>

lib/bup/client.py
@@ -9,7 +9,7 @@ from bup import git, ssh, vfs
 from bup.compat import environ, range, reraise
 from bup.helpers import (Conn, atomically_replaced_file, chunkyreader, debug1,
                          debug2, linereader, lines_until_sentinel,
-                         mkdirp, progress, qprogress, DemuxConn)
+                         mkdirp, progress, qprogress, DemuxConn, atoi)
 from bup.io import path_msg
 from bup.vint import read_bvec, read_vuint, write_bvec
 