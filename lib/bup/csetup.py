ada27449c88b758c84a2a310f423ecfb69785bec
Johannes Berg <johannes@sipsolutions.net>
2020-02-05 20:36:46 +0100

    build: fix C-side dependencies
    We need to depend on bupsplit.h, otherwise changes there
    don't force a rebuild.
    
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    Reviewed-by: Rob Browning <rlb@defaultvalue.org>
    (cherry picked from commit 82d951a8589b8bd5a3e61b4d0a7b70e8ef6f6231)

--- lib/bup/csetup.py
+++ lib/bup/csetup.py
@@ -5,7 +5,7 @@ from distutils.core import setup, Extension
 
 _helpers_mod = Extension('_helpers',
                          sources=['_helpers.c', 'bupsplit.c'],
-                         depends=['../../config/config.h'])
+                         depends=['../../config/config.h', 'bupsplit.h'])
 
 setup(name='_helpers',
       version='0.1',
