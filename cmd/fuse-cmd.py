bup/bup

commit f76c37383ddb82343e85632e9154547412e7b1ad

Remove vfs (replaced by vfs2)
Signed-off-by: Rob Browning <rlb@defaultvalue.org>
Tested-by: Rob Browning <rlb@defaultvalue.org>

cmd/fuse-cmd.py
@@ -16,7 +16,7 @@ if not hasattr(fuse, '__version__'):
     raise RuntimeError, "your fuse module is too old for fuse.__version__"
 fuse.fuse_python_api = (0, 2)
 
-from bup import options, git, vfs2 as vfs, xstat
+from bup import options, git, vfs, xstat
 from bup.helpers import log
 from bup.repo import LocalRepo
 

lib/bup/t/tvfs.py
@@ -10,7 +10,7 @@ from time import localtime, strftime
 
 from wvtest import *
 
-from bup import git, metadata, vfs2 as vfs
+from bup import git, metadata, vfs
 from bup.git import BUP_CHUNKED
 from bup.helpers import exc, exo, shstr
 from bup.metadata import Metadata

cmd/cat-file-cmd.py
@@ -7,8 +7,7 @@ exec "$bup_python" "$0" ${1+"$@"}
 
 import re, stat, sys
 
-from bup import options, git
-from bup import vfs2 as vfs
+from bup import options, git, vfs
 from bup.helpers import chunkyreader, handle_ctrl_c, log, saved_errors
 from bup.repo import LocalRepo
 

cmd/ftp-cmd.py
@@ -7,7 +7,7 @@ exec "$bup_python" "$0" ${1+"$@"}
 
 import sys, os, stat, fnmatch
 
-from bup import options, git, shquote, ls, vfs2 as vfs
+from bup import options, git, shquote, ls, vfs
 from bup.helpers import chunkyreader, handle_ctrl_c, log
 from bup.repo import LocalRepo
 

cmd/restore-cmd.py
@@ -9,7 +9,7 @@ from __future__ import print_function
 from stat import S_ISDIR
 import copy, errno, os, sys, stat, re
 
-from bup import options, git, metadata, vfs2
+from bup import options, git, metadata, vfs
 from bup._helpers import write_sparsely
 from bup.compat import wrap_main
 from bup.helpers import (add_error, chunkyreader, die_if_errors, handle_ctrl_c,
@@ -133,13 +133,13 @@ def hardlink_if_possible(fullname, item, top, hardlinks):
     return False
 
 def write_file_content(repo, dest_path, vfs_file):
-    with vfs2.fopen(repo, vfs_file) as inf:
+    with vfs.fopen(repo, vfs_file) as inf:
         with open(dest_path, 'wb') as outf:
             for b in chunkyreader(inf):
                 outf.write(b)
 
 def write_file_content_sparsely(repo, dest_path, vfs_file):
-    with vfs2.fopen(repo, vfs_file) as inf:
+    with vfs.fopen(repo, vfs_file) as inf:
         outfd = os.open(dest_path, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0o600)
         try:
             trailing_zeros = 0;
@@ -153,7 +153,7 @@ def write_file_content_sparsely(repo, dest_path, vfs_file):
 def restore(repo, parent_path, name, item, top, sparse, numeric_ids, owner_map,
             exclude_rxs, verbosity, hardlinks):
     global total_restored
-    mode = vfs2.item_mode(item)
+    mode = vfs.item_mode(item)
     treeish = S_ISDIR(mode)
     fullname = parent_path + '/' + name
     # Match behavior of index --exclude-rx with respect to paths.
@@ -163,7 +163,7 @@ def restore(repo, parent_path, name, item, top, sparse, numeric_ids, owner_map,
 
     if not treeish:
         # Do this now so we'll have meta.symlink_target for verbose output
-        item = vfs2.augment_item_meta(repo, item, include_size=True)
+        item = vfs.augment_item_meta(repo, item, include_size=True)
         meta = item.meta
         assert(meta.mode == mode)
 
@@ -182,10 +182,10 @@ def restore(repo, parent_path, name, item, top, sparse, numeric_ids, owner_map,
     try:
         if treeish:
             # Assumes contents() returns '.' with the full metadata first
-            sub_items = vfs2.contents(repo, item, want_meta=True)
+            sub_items = vfs.contents(repo, item, want_meta=True)
             dot, item = next(sub_items, None)
             assert(dot == '.')
-            item = vfs2.augment_item_meta(repo, item, include_size=True)
+            item = vfs.augment_item_meta(repo, item, include_size=True)
             meta = item.meta
             meta.create_path(name)
             os.chdir(name)
@@ -246,8 +246,8 @@ def main():
             add_error("path %r doesn't include a branch and revision" % path)
             continue
         try:
-            resolved = vfs2.lresolve(repo, path, want_meta=True)
-        except vfs2.IOError as e:
+            resolved = vfs.lresolve(repo, path, want_meta=True)
+        except vfs.IOError as e:
             add_error(e)
             continue
         path_parent, path_name = os.path.split(path)
@@ -262,11 +262,11 @@ def main():
             # what/ever/* to the current directory, and if name == '.'
             # (i.e. /foo/what/ever/.), then also restore what/ever's
             # metadata to the current directory.
-            treeish = vfs2.item_mode(leaf_item)
+            treeish = vfs.item_mode(leaf_item)
             if not treeish:
                 add_error('%r cannot be restored as a directory' % path)
             else:
-                items = vfs2.contents(repo, leaf_item, want_meta=True)
+                items = vfs.contents(repo, leaf_item, want_meta=True)
                 dot, leaf_item = next(items, None)
                 assert(dot == '.')
                 for sub_name, sub_item in items:
@@ -274,8 +274,8 @@ def main():
                             opt.sparse, opt.numeric_ids, owner_map,
                             exclude_rxs, verbosity, hardlinks)
                 if path_name == '.':
-                    leaf_item = vfs2.augment_item_meta(repo, leaf_item,
-                                                       include_size=True)
+                    leaf_item = vfs.augment_item_meta(repo, leaf_item,
+                                                      include_size=True)
                     apply_metadata(leaf_item.meta, '.',
                                    opt.numeric_ids, owner_map)
         else:

cmd/web-cmd.py
@@ -9,7 +9,7 @@ from __future__ import print_function
 from collections import namedtuple
 import mimetypes, os, posixpath, signal, stat, sys, time, urllib, webbrowser
 
-from bup import options, git, vfs2 as vfs
+from bup import options, git, vfs
 from bup.helpers import (chunkyreader, debug1, format_filesize, handle_ctrl_c,
                          log, resource_path, saved_errors)
 from bup.metadata import Metadata

lib/bup/ls.py
@@ -5,7 +5,7 @@ from itertools import chain
 from stat import S_ISDIR, S_ISLNK
 import copy, locale, os.path, stat, sys, xstat
 
-from bup import metadata, options, vfs2 as vfs
+from bup import metadata, options, vfs
 from bup.options import Options
 from bup.repo import LocalRepo, RemoteRepo
 from helpers import columnate, istty1, last, log

commit f86e3d704af40bf98a557e58f634b1c917019821

vfs: use None for unknown uid/gid
This means we show '?' instead of 0 for unknown UIDs when
numeric output is requested, as it was before.

This also uncovered a forgotten bytes annotation for the
"unknown" string ('?' should be b'?').

Somehow, this new behaviour (of printing 0 instead of ?)
also got quite enshrined in the test suite, fix that too.

And finally, on python 2, fuse doesn't accept None in the
stat struct (but does on python 3, go figure).

Fixes: f76c37383ddb ("Remove vfs (replaced by vfs2)")
Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
Reviewed-by: Rob Browning <rlb@defaultvalue.org>

cmd/fuse-cmd.py
@@ -69,8 +69,8 @@ class BupFs(fuse.Fuse):
         # FIXME: do we want/need to do anything more with nlink?
         st = fuse.Stat(st_mode=meta.mode, st_nlink=1, st_size=meta.size)
         st.st_mode = meta.mode
-        st.st_uid = meta.uid
-        st.st_gid = meta.gid
+        st.st_uid = meta.uid or 0
+        st.st_gid = meta.gid or 0
         st.st_atime = max(0, xstat.fstime_floor_secs(meta.atime))
         st.st_mtime = max(0, xstat.fstime_floor_secs(meta.mtime))
         st.st_ctime = max(0, xstat.fstime_floor_secs(meta.ctime))

lib/bup/metadata.py
@@ -418,8 +418,10 @@ class Metadata:
 
         uid = gid = -1 # By default, do nothing.
         if is_superuser():
-            uid = self.uid
-            gid = self.gid
+            if self.uid is not None:
+                uid = self.uid
+            if self.gid is not None:
+                gid = self.gid
             if not restore_numeric_ids:
                 if self.uid != 0 and self.user:
                     entry = pwd_from_name(self.user)
@@ -1012,7 +1014,7 @@ def summary_bytes(meta, numeric_ids = False, classification = None,
                   human_readable = False):
     """Return bytes containing the "ls -l" style listing for meta.
     Classification may be "all", "type", or None."""
-    user_str = group_str = size_or_dev_str = '?'
+    user_str = group_str = size_or_dev_str = b'?'
     symlink_target = None
     if meta:
         name = meta.path

lib/bup/vfs.py
@@ -1112,7 +1112,8 @@ def augment_item_meta(repo, item, include_size=False):
     # m is mode
     meta = Metadata()
     meta.mode = m
-    meta.uid = meta.gid = meta.atime = meta.mtime = meta.ctime = 0
+    meta.uid = meta.gid = None
+    meta.atime = meta.mtime = meta.ctime = 0
     if S_ISLNK(m):
         if isinstance(item, FakeLink):
             target = item.target

lib/bup/t/tvfs.py
@@ -114,7 +114,7 @@ def run_augment_item_meta_tests(repo,
         wvpass(item is not file_item)
         wvpass(isinstance(meta, Metadata))
         wvpasseq(vfs.default_file_mode, meta.mode)
-        wvpasseq((0, 0, 0, 0, 0),
+        wvpasseq((None, None, 0, 0, 0),
                  (meta.uid, meta.gid, meta.atime, meta.mtime, meta.ctime))
     wvpass(augmented.meta.size is None)
     wvpasseq(file_size, augmented_w_size.meta.size)

t/test-ls
@@ -136,23 +136,23 @@ WVPASSEQ "$(WVPASS bup-ls -l / | tr -s ' ' ' ')" \
 "drwx------ $user/$group 0 2009-10-03 23:48 src"
 
 WVPASSEQ "$(WVPASS bup-ls -lA / | tr -s ' ' ' ')" \
-"drwxr-xr-x 0/0 0 1970-01-01 00:00 .tag
+"drwxr-xr-x ?/? 0 1970-01-01 00:00 .tag
 drwx------ $user/$group 0 2009-10-03 23:48 src"
 
 WVPASSEQ "$(WVPASS bup-ls -lAF / | tr -s ' ' ' ')" \
-"drwxr-xr-x 0/0 0 1970-01-01 00:00 .tag/
+"drwxr-xr-x ?/? 0 1970-01-01 00:00 .tag/
 drwx------ $user/$group 0 2009-10-03 23:48 src/"
 
 WVPASSEQ "$(WVPASS bup-ls -la / | tr -s ' ' ' ')" \
-"drwxr-xr-x 0/0 0 1970-01-01 00:00 .
-drwxr-xr-x 0/0 0 1970-01-01 00:00 ..
-drwxr-xr-x 0/0 0 1970-01-01 00:00 .tag
+"drwxr-xr-x ?/? 0 1970-01-01 00:00 .
+drwxr-xr-x ?/? 0 1970-01-01 00:00 ..
+drwxr-xr-x ?/? 0 1970-01-01 00:00 .tag
 drwx------ $user/$group 0 2009-10-03 23:48 src"
 
 WVPASSEQ "$(WVPASS bup-ls -laF / | tr -s ' ' ' ')" \
-"drwxr-xr-x 0/0 0 1970-01-01 00:00 ./
-drwxr-xr-x 0/0 0 1970-01-01 00:00 ../
-drwxr-xr-x 0/0 0 1970-01-01 00:00 .tag/
+"drwxr-xr-x ?/? 0 1970-01-01 00:00 ./
+drwxr-xr-x ?/? 0 1970-01-01 00:00 ../
+drwxr-xr-x ?/? 0 1970-01-01 00:00 .tag/
 drwx------ $user/$group 0 2009-10-03 23:48 src/"
 
 socket_mode="$(WVPASS ls -l src/socket | cut -b -10)" || exit $?
@@ -252,14 +252,14 @@ $socket_mode $uid/$gid 0 2009-10-03 23:48 socket
 $symlink_mode $uid/$gid $symlink_size $symlink_date symlink -> file"
 
 WVPASSEQ "$(bup-ls -ld "src/latest" | tr -s ' ' ' ')" \
-"lrwxr-xr-x 0/0 17 1970-01-01 00:00 src/latest -> 1977-09-05-125600"
+"lrwxr-xr-x ?/? 17 1970-01-01 00:00 src/latest -> 1977-09-05-125600"
 
 
 WVSTART "$ls_cmd_desc (backup set - long)"
 WVPASSEQ "$(bup-ls -l --numeric-ids src | cut -d' ' -f 1-2)" \
 "drwx------ $uid/$gid
 drwx------ $uid/$gid
-lrwxr-xr-x 0/0"
+lrwxr-xr-x ?/?"
 
 WVPASSEQ "$(bup-ls -ds "src/1977-09-05-125600" | tr -s ' ' ' ')" \
 "$src_tree_hash src/1977-09-05-125600"