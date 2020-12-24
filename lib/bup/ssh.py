bup/bup

commit cd3647c81a25e9a512710040fb70d080656266e6

bup: pass TTY width through to sub-command and remote
As we rewrite the sub-command's stderr, it cannot determine
the width. As a result, e.g. "bup ls" will always columnate
only to 70 columns, instead of using the entire width. Fix
this by passing the real width through in an environment
variable, just like we already pass through the fact that
it's a TTY.

Also pass it through on the ssh command line so that if you
run 'bup on ... ls' it actually behaves right.

Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
Reviewed-by: Rob Browning <rlb@defaultvalue.org>
Tested-by: Rob Browning <rlb@defaultvalue.org>

lib/bup/ssh.py
@@ -16,9 +16,10 @@ def connect(rhost, port, subcmd, stderr=None):
     else:
         buglvl = helpers.atoi(environ.get(b'BUP_DEBUG'))
         force_tty = helpers.atoi(environ.get(b'BUP_FORCE_TTY'))
+        tty_width = helpers.atoi(environ.get(b'BUP_TTY_WIDTH'))
         cmd = b"""
-                   sh -c 'BUP_DEBUG=%d BUP_FORCE_TTY=%d bup %s'
-               """ % (buglvl, force_tty, subcmd)
+                   sh -c 'BUP_DEBUG=%d BUP_FORCE_TTY=%d BUP_TTY_WIDTH=%d bup %s'
+               """ % (buglvl, force_tty, tty_width, subcmd)
         argv = [b'ssh']
         if port:
             argv.extend((b'-p', port))

lib/bup/options.py
@@ -134,6 +134,9 @@ if not fcntl and termios:
         return 70
 else:
     def _tty_width():
+        forced = os.environ.get('BUP_TTY_WIDTH', None)
+        if forced:
+            return int(forced)
         s = struct.pack("HHHH", 0, 0, 0, 0)
         try:
             s = fcntl.ioctl(sys.stderr.fileno(), termios.TIOCGWINSZ, s)

lib/cmd/bup
@@ -39,6 +39,7 @@ from bup import compat, path, helpers
 from bup.compat import add_ex_tb, add_ex_ctx, argv_bytes, wrap_main
 from bup.helpers import atoi, columnate, debug1, log, merge_dict, tty_width
 from bup.io import byte_stream, path_msg
+from bup.options import _tty_width
 
 cmdpath = path.cmddir()
 
@@ -154,7 +155,8 @@ if fix_stdout or fix_stderr:
     tty_env = merge_dict(environ,
                          {b'BUP_FORCE_TTY': (b'%d'
                                              % ((fix_stdout and 1 or 0)
-                                                + (fix_stderr and 2 or 0)))})
+                                                + (fix_stderr and 2 or 0))),
+                          b'BUP_TTY_WIDTH': b'%d' % _tty_width(), })
 else:
     tty_env = environ
 

commit d55be20f5d1efdfbc7bac80bcb1a5f4825872f62

ssh: don't pass through bad BUP_TTY_WIDTH
If there's no BUP_TTY_WIDTH variable already, then environ.get()
will result in None, which cannot be converted to an integer so
we get 0. Then we pass 0 to the other side in the BUP_TTY_WIDTH
environment variable.

Unfortunately, 0 is a really bad default, since textwrap cannot
work with a width of 0, causing tracebacks such as the one Mark
J Hewitt reported:
https://groups.google.com/g/bup-list/c/rfIw3STN2EY/m/C5Y3UrMIAgAJ

Fix this by not passing it through if there's no good value.

Reported-by: Mark J Hewitt <mjh@idnet.com>
Reported-by: Alexander Barton <alex@barton.de>
Fixes: cd3647c81a25 ("bup: pass TTY width through to sub-command and remote")
Reviewed-by: Rob Browning <rlb@defaultvalue.org>
Tested-by: Rob Browning <rlb@defaultvalue.org>

lib/bup/ssh.py
@@ -16,9 +16,13 @@ def connect(rhost, port, subcmd, stderr=None):
     else:
         buglvl = helpers.atoi(environ.get(b'BUP_DEBUG'))
         force_tty = helpers.atoi(environ.get(b'BUP_FORCE_TTY'))
-        tty_width = helpers.atoi(environ.get(b'BUP_TTY_WIDTH'))
+        tty_width = environ.get(b'BUP_TTY_WIDTH', None)
+        if tty_width is not None:
+            tty_width = b'BUP_TTY_WIDTH=%d' % helpers.atoi(tty_width)
+        else:
+            tty_width = b''
         cmd = b"""
-                   sh -c 'BUP_DEBUG=%d BUP_FORCE_TTY=%d BUP_TTY_WIDTH=%d bup %s'
+                   sh -c 'BUP_DEBUG=%d BUP_FORCE_TTY=%d %s bup %s'
                """ % (buglvl, force_tty, tty_width, subcmd)
         argv = [b'ssh']
         if port: