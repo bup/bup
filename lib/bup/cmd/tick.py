d2fc6838eae23264c5a4565c553b648792e5b53a
Rob Browning <rlb@defaultvalue.org>
2021-02-12 13:36:04 -0600

    tick: convert to internal command
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>

--- lib/bup/cmd/tick.py
+++ lib/bup/cmd/tick.py
@@ -1,39 +1,21 @@
-#!/bin/sh
-"""": # -*-python-*-
-# https://sourceware.org/bugzilla/show_bug.cgi?id=26034
-export "BUP_ARGV_0"="$0"
-arg_i=1
-for arg in "$@"; do
-    export "BUP_ARGV_${arg_i}"="$arg"
-    shift
-    arg_i=$((arg_i + 1))
-done
-# Here to end of preamble replaced during install
-bup_python="$(dirname "$0")/../../../config/bin/python" || exit $?
-exec "$bup_python" "$0"
-"""
-# end of bup preamble
 
 from __future__ import absolute_import
+import os, sys, time
 
-# Intentionally replace the dirname "$0" that python prepends
-import os, sys
-sys.path[0] = os.path.dirname(os.path.realpath(__file__)) + '/../..'
-
-import time
-
-from bup import compat, options
+from bup import options
 
 
 optspec = """
 bup tick
 """
-o = options.Options(optspec)
-opt, flags, extra = o.parse(compat.argv[1:])
 
-if extra:
-    o.fatal("no arguments expected")
+def main(argv):
+    o = options.Options(optspec)
+    opt, flags, extra = o.parse_bytes(argv[1:])
 
-t = time.time()
-tleft = 1 - (t - int(t))
-time.sleep(tleft)
+    if extra:
+        o.fatal("no arguments expected")
+
+    t = time.time()
+    tleft = 1 - (t - int(t))
+    time.sleep(tleft)
