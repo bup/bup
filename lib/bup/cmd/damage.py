3ad4502c8fe8e7bb6e0ee1dbe25254f03c8c8403
Rob Browning <rlb@defaultvalue.org>
2021-02-12 13:51:46 -0600

    damage: convert to internal command
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>

--- lib/bup/cmd/damage.py
+++ lib/bup/cmd/damage.py
@@ -1,28 +1,8 @@
-#!/bin/sh
-"""": # -*-python-*-
-# https://sourceware.org/bugzilla/show_bug.cgi?id=26034
-export "BUP_ARGV_0"="$0"
-arg_i=1
-for arg in "$@"; do
-    export "BUP_ARGV_${arg_i}"="$arg"
-    shift
-    arg_i=$((arg_i + 1))
-done
-# Here to end of preamble replaced during install
-bup_python="$(dirname "$0")/../../../config/bin/python" || exit $?
-exec "$bup_python" "$0"
-"""
-# end of bup preamble
 
 from __future__ import absolute_import
+import os, random, sys
 
-# Intentionally replace the dirname "$0" that python prepends
-import os, sys
-sys.path[0] = [os.path.dirname(os.path.realpath(__file__)) + '/../..']
-
-import random
-
-from bup import compat, options
+from bup import options
 from bup.compat import argv_bytes, bytes_from_uint, range
 from bup.helpers import log
 from bup.io import path_msg
@@ -42,37 +22,39 @@ percent= maximum size of each damaged block (as a percent of entire file)
 equal    spread damage evenly throughout the file
 S,seed=  random number seed (for repeatable tests)
 """
-o = options.Options(optspec)
-opt, flags, extra = o.parse(compat.argv[1:])
 
-if not extra:
-    o.fatal('filenames expected')
+def main(argv):
+    o = options.Options(optspec)
+    opt, flags, extra = o.parse_bytes(argv[1:])
 
-if opt.seed != None:
-    random.seed(opt.seed)
+    if not extra:
+        o.fatal('filenames expected')
 
-for name in extra:
-    name = argv_bytes(name)
-    log('Damaging "%s"...\n' % path_msg(name))
-    with open(name, 'r+b') as f:
-        st = os.fstat(f.fileno())
-        size = st.st_size
-        if opt.percent or opt.size:
-            ms1 = int(float(opt.percent or 0)/100.0*size) or size
-            ms2 = opt.size or size
-            maxsize = min(ms1, ms2)
-        else:
-            maxsize = 1
-        chunks = opt.num or 10
-        chunksize = size // chunks
-        for r in range(chunks):
-            sz = random.randrange(1, maxsize+1)
-            if sz > size:
-                sz = size
-            if opt.equal:
-                ofs = r*chunksize
+    if opt.seed != None:
+        random.seed(opt.seed)
+
+    for name in extra:
+        name = argv_bytes(name)
+        log('Damaging "%s"...\n' % path_msg(name))
+        with open(name, 'r+b') as f:
+            st = os.fstat(f.fileno())
+            size = st.st_size
+            if opt.percent or opt.size:
+                ms1 = int(float(opt.percent or 0)/100.0*size) or size
+                ms2 = opt.size or size
+                maxsize = min(ms1, ms2)
             else:
-                ofs = random.randrange(0, size - sz + 1)
-            log('  %6d bytes at %d\n' % (sz, ofs))
-            f.seek(ofs)
-            f.write(randblock(sz))
+                maxsize = 1
+            chunks = opt.num or 10
+            chunksize = size // chunks
+            for r in range(chunks):
+                sz = random.randrange(1, maxsize+1)
+                if sz > size:
+                    sz = size
+                if opt.equal:
+                    ofs = r*chunksize
+                else:
+                    ofs = random.randrange(0, size - sz + 1)
+                log('  %6d bytes at %d\n' % (sz, ofs))
+                f.seek(ofs)
+                f.write(randblock(sz))
