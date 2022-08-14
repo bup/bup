3914d75c42f60d71ace4957bd31d3009d9f8ed0c
Rob Browning <rlb@defaultvalue.org>
2022-08-14 13:29:59 -0500

    stat_result: specify __slots__
    We're likely to have a lot of these, so might as well save the memory.
    
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- lib/bup/xstat.py
+++ lib/bup/xstat.py
@@ -102,6 +102,8 @@ def _fix_cygwin_id(id):
 
 
 class stat_result:
+    __slots__ = ('st_mode', 'st_ino', 'st_dev', 'st_nlink', 'st_uid', 'st_gid',
+                 'st_rdev', 'st_size', 'st_atime', 'st_mtime', 'st_ctime')
     @staticmethod
     def from_xstat_rep(st):
         global _cygwin_sys
