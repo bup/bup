a7d9c28eeb1e5e2b69697348d65c8de5c376f93c
Johannes Berg <johannes@sipsolutions.net>
2024-01-07 00:14:48 +0100

    git_config_get: don't require repo with cfg_file
    Fixes: 6c5edf4adb908097b734e456f692623a8a8ca962
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
    [rlb@defaultvalue.org: create inital cmd in single assignment]
    Reviewed-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- lib/bup/git.py
+++ lib/bup/git.py
@@ -70,9 +70,10 @@ def _git_exo(cmd, **kwargs):
 
 def git_config_get(option, repo_dir=None, opttype=None, cfg_file=None):
     assert not (repo_dir and cfg_file), "repo_dir and cfg_file cannot both be used"
-    cmd = [b'git', b'--git-dir', repo_dir or repo(), b'config', b'--null']
     if cfg_file:
-        cmd.extend([b'--file', cfg_file])
+        cmd = [b'git', b'config', b'--file', cfg_file, b'--null']
+    else:
+        cmd = [b'git', b'--git-dir', repo_dir or repo(), b'config', b'--null']
     if opttype == 'int':
         cmd.extend([b'--int'])
     elif opttype == 'bool':
