bup/bup

commit fabb6911ccde94cb6a7a588c71f9b29f4a024d5b

git_config_get: restore default to repo()
In 0.33.x this stopped defaulting to repo(), and so no longer
respected BUP_DIR.  Adjust the code to fix that, and instead of
overriding the environment via _gitenv, just directly specify the
--git-dir we want.

Signed-off-by: Rob Browning <rlb@defaultvalue.org>
Tested-by: Rob Browning <rlb@defaultvalue.org>
(cherry picked from commit 6c5edf4adb908097b734e456f692623a8a8ca962)

lib/bup/git.py
@@ -49,6 +49,7 @@ class GitError(Exception):
 
 
 def _gitenv(repo_dir=None):
+    # This is not always used, i.e. sometimes we just use --git-dir
     if not repo_dir:
         repo_dir = repo()
     return merge_dict(environ, {b'GIT_DIR': os.path.abspath(repo_dir)})
@@ -68,7 +69,7 @@ def _git_exo(cmd, **kwargs):
 
 def git_config_get(option, repo_dir=None, opttype=None, cfg_file=None):
     assert not (repo_dir and cfg_file), "repo_dir and cfg_file cannot both be used"
-    cmd = [b'git', b'config', b'--null']
+    cmd = [b'git', b'--git-dir', repo_dir or repo(), b'config', b'--null']
     if cfg_file:
         cmd.extend([b'--file', cfg_file])
     if opttype == 'int':
@@ -79,10 +80,7 @@ def git_config_get(option, repo_dir=None, opttype=None, cfg_file=None):
         assert opttype is None
     cmd.extend([b'--get', option])
     env=None
-    if repo_dir:
-        env = _gitenv(repo_dir=repo_dir)
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env,
-                         close_fds=True)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, close_fds=True)
     # with --null, git writes out a trailing \0 after the value
     r = p.stdout.read()[:-1]
     rc = p.wait()

test/int/test_git.py
@@ -541,7 +541,7 @@ def test_midx_close(tmpdir):
             # check that we don't have it open anymore
             WVPASSEQ(False, b'deleted' in fn)
 
-def test_config():
+def test_config(tmpdir):
     cfg_file = os.path.join(os.path.dirname(__file__), 'sample.conf')
     no_such_file = os.path.join(os.path.dirname(__file__), 'nosuch.conf')
     git_config_get = partial(git.git_config_get, cfg_file=cfg_file)
@@ -568,3 +568,10 @@ def test_config():
     WVPASSEQ(2, git_config_get(b'bup.istrue2', opttype='int'))
     WVPASSEQ(0, git_config_get(b'bup.isfalse2', opttype='int'))
     WVPASSEQ(0x777, git_config_get(b'bup.hex', opttype='int'))
+
+    # Make sure get_config respects the repo()
+    git_dir = tmpdir + b'/repo'
+    git.init_repo(git_dir)
+    git.check_repo_or_die(git_dir)
+    exc(b'git', b'--git-dir', git_dir, b'config', b'bup.foo', b'yep')
+    assert b'yep' == git.git_config_get(b'bup.foo')

commit 87b697ff0311b8567354e28a248d7eefcd40fc83

git_config_get: don't require repo with cfg_file
Fixes: fabb6911ccde ("git_config_get: restore default to repo()")
Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
[rlb@defaultvalue.org: create iniital cmd in single assignment]
Reviewed-by: Rob Browning <rlb@defaultvalue.org>
Tested-by: Rob Browning <rlb@defaultvalue.org>

lib/bup/git.py
@@ -69,9 +69,10 @@ def _git_exo(cmd, **kwargs):
 
 def git_config_get(option, repo_dir=None, opttype=None, cfg_file=None):
     assert not (repo_dir and cfg_file), "repo_dir and cfg_file cannot both be used"
-    cmd = [b'git', b'--git-dir', repo_dir or repo(), b'config', b'--null']
     if cfg_file:
-        cmd.extend([b'--file', cfg_file])
+        cmd = [b'git', b'config', b'--file', cfg_file, b'--null']
+    else:
+        cmd = [b'git', b'--git-dir', repo_dir or repo(), b'config', b'--null']
     if opttype == 'int':
         cmd.extend([b'--int'])
     else: