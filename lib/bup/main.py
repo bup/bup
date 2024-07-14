29e131acbfa8dab6380a5c73a85f7b6a603a2c83
Rob Browning <rlb@defaultvalue.org>
2024-07-14 14:27:10 -0500

    main: treat missing submodule as error, not unknown command
    Previously, if a command module like bup.web was missing an import,
    we'd treat it as if "bup web" was an unknown command.  Now we'll just
    throw the relevant exception.
    
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- lib/bup/main.py
+++ lib/bup/main.py
@@ -176,9 +176,11 @@ if not subcmd_name:
     usage()
 
 try:
-    cmd_module = import_module('bup.cmd.'
-                               + subcmd_name.decode('ascii').replace('-', '_'))
+    cmd_module_name = 'bup.cmd.' + subcmd_name.decode('ascii').replace('-', '_')
+    cmd_module = import_module(cmd_module_name)
 except ModuleNotFoundError as ex:
+    if ex.name != cmd_module_name:
+        raise ex
     cmd_module = None
 
 if not cmd_module:
