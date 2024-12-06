bup/bup

commit e23d317c73e966234b5a6b86cdfe0e65fd4e9ed0

Suppress the git main branch name message on bup init
If the git config option init.defaultBranch is not set, git init
prints a message on how to change the default branch name. The default
branch name is never used, so the message is irrelevant and might be
confusing for users.  Setting some explicit default branch name
suppresses that warning.

The message might not appear for most frequent git users, as they will
have set git config --global init.defaultBranch at some point to get
rid of this message on git init. However, it will affect most new
users and users that use only bup, not git. It is therefore an UX
improvement.

Signed-off-by: Moritz Lell <mlell08@gmail.com>

lib/bup/git.py
@@ -1200,7 +1200,10 @@ def init_repo(path=None):
                        % path_msg(parent))
     if os.path.exists(d) and not os.path.isdir(os.path.join(d, b'.')):
         raise GitError('"%s" exists but is not a directory\n' % path_msg(d))
-    p = subprocess.Popen([b'git', b'--bare', b'init'], stdout=sys.stderr,
+    p = subprocess.Popen([ b'git', b'--bare',
+                           # arbitrary default branch name to suppress git msg.
+                           b'-c', b'init.defaultBranch=main', b'init'],
+                         stdout=sys.stderr,
                          env=_gitenv(),
                          close_fds=True)
     _git_wait('git init', p)

commit bc87f7776f2e4c36ed0ca33116f8580d29198314

git: bump minimum version to 1.7.2
Since the commit mentioned below, we need to have at least
git version 1.7.2 since before that it didn't know the -c
option.

Fixes: e23d317c73e9 ("Suppress the git main branch name message on bup init")
Signed-off-by: Johannes Berg <johannes@sipsolutions.net>
Reviewed-by: Rob Browning <rlb@defaultvalue.org>
Tested-by: Rob Browning <rlb@defaultvalue.org>

lib/bup/git.py
@@ -1314,13 +1314,13 @@ def is_suitable_git(ver_str):
     if ver_str.startswith(b'0.'):
         return 'insufficient'
     if ver_str.startswith(b'1.'):
-        if re.match(br'1\.[012345]rc', ver_str):
+        if re.match(br'1\.[01234567]rc', ver_str):
             return 'insufficient'
-        if re.match(br'1\.[01234]\.', ver_str):
+        if re.match(br'1\.[0123456]\.', ver_str):
             return 'insufficient'
-        if re.match(br'1\.5\.[012345]($|\.)', ver_str):
+        if re.match(br'1\.7\.[01]($|\.)', ver_str):
             return 'insufficient'
-        if re.match(br'1\.5\.6-rc', ver_str):
+        if re.match(br'1\.7\.2-rc', ver_str):
             return 'insufficient'
         return 'suitable'
     if re.match(br'[0-9]+(\.|$)?', ver_str):
@@ -1349,7 +1349,7 @@ def require_suitable_git(ver_str=None):
     if status == 'unrecognized':
         raise GitError('Unexpected git --version output: %r' % ver_str)
     if status == 'insufficient':
-        log('error: git version must be at least 1.5.6\n')
+        log('error: git version must be at least 1.7.2\n')
         sys.exit(1)
     if status == 'suitable':
         _git_great = True

test/int/test_git.py
@@ -38,10 +38,10 @@ def test_git_version_detection():
          ('insufficient', b'git version 1.0rc1'),
          ('insufficient', b'git version 1.0.1'),
          ('insufficient', b'git version 1.4.2.1'),
-         ('insufficient', b'git version 1.5.5'),
-         ('insufficient', b'git version 1.5.6-rc0'),
-         ('suitable', b'git version 1.5.6'),
-         ('suitable', b'git version 1.5.6.1'),
+         ('insufficient', b'git version 1.7.1'),
+         ('insufficient', b'git version 1.7.2-rc0'),
+         ('suitable', b'git version 1.7.2'),
+         ('suitable', b'git version 1.7.2.1'),
          ('suitable', b'git version 2.14.0-rc0'),
          ('suitable', b'git version 2.14.0 (something ...)'),
          ('suitable', b'git version 111.222.333.444-rc555'),