f7a8316efac3bd842ccae1bece5bc16367e9fc4b
Rob Browning <rlb@defaultvalue.org>
2025-07-30 15:16:20 -0500

    rewrite: avoid redundant work (e.g. vfs resolves)
    When walking the source tree, yield paths that have the correct/full
    metadata for all the items, including the directory itself if the path
    is a directory. This allows us to drop the calls to resolve() the
    metadata.
    
    When we encounter a directory that's already been converted, just
    provide the oid so we can call append_to_current() directly to add it
    to the parent.
    
    Thanks to Johannes Berg for investigating and diagnosing the problem
    and then devising a similar solution that lead directly to this one.
    
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- lib/bup/rewrite.py
+++ lib/bup/rewrite.py
@@ -5,19 +5,14 @@ from itertools import chain
 from os.path import join as joinp
 from stat import S_ISDIR, S_ISLNK, S_ISREG
 from typing import Any, Sequence
-import os, sqlite3, time
+import sqlite3, time
 
 from bup import hashsplit, metadata, vfs
 from bup.commit import commit_message
 from bup.compat import dataclass
 from bup.git import get_cat_data, parse_commit
 from bup.hashsplit import GIT_MODE_FILE, GIT_MODE_SYMLINK, GIT_MODE_TREE
-from bup.helpers import \
-    (hostname,
-     log,
-     path_components,
-     should_rx_exclude_path,
-     temp_dir)
+from bup.helpers import hostname, log, should_rx_exclude_path, temp_dir
 from bup.io import path_msg, qsql_id
 from bup.metadata import Metadata
 from bup.pwdgrp import userfullname, username
@@ -152,8 +147,12 @@ class IncompleteDir:
     missing: bytes # MissingObject oid
 
 def _vfs_walk_dir_recursively(srcrepo, dstrepo, path, excludes, db, mapping,
-                              missing):
-    """Yield the paths underneath the given path.
+                              missing, *, _replacement_parents=None):
+    """Yield information about the paths underneath the given path.
+
+    Yield (src_path, replacement_dir), where src_path is a vfs_path
+    and replacement_dir is be the replacement tree oid for a src_path
+    representing a directory that has already been rewritten.
 
     When unreadable objects are encountered, raise MissingObject if
     missing.mode is 'fail', otherwise, for missing.mode 'replace',
@@ -163,6 +162,9 @@ def _vfs_walk_dir_recursively(srcrepo, dstrepo, path, excludes, db, mapping,
     """
     assert isinstance(missing, MissingConfig), missing
     assert missing.mode in ('fail', 'replace'), missing
+    if _replacement_parents is None:
+        _replacement_parents = tuple([])
+
     item = path[-1][1]
     assert len(path) >= 3
     # drop branch/DATE
@@ -179,32 +181,48 @@ def _vfs_walk_dir_recursively(srcrepo, dstrepo, path, excludes, db, mapping,
             # itself isn't broken; its contents may be.
             entries = list(vfs.contents(srcrepo, item))
         except MissingObject as ex:
-            yield IncompleteDir(path, ex.oid)
+            yield IncompleteDir(path, ex.oid), None
             return
+
+    path_w_meta = None
     for entry in entries:
         name, sub_item = entry
-        sub_path = path + (entry,)
-        if name in (b'.', b'..'):
+        if name == b'.':
+            # contents() promises this
+            assert path_w_meta is None, 'two "." dir entries encountered?!'
+            # Create version of path with its real metadata, not the
+            # contents() placeholder mode for dirs.
+            assert isinstance(entry[1].meta, Metadata), entry
+            dir_name, dir_item = path[-1]
+            path_w_meta = path[:-1] \
+                + ((dir_name, dir_item._replace(meta=entry[1].meta)),)
             continue
         sub_fs_path_in_save = joinp(fs_path_in_save, name)
         if S_ISDIR(vfs.item_mode(sub_item)):
             sub_fs_path_in_save += b'/'
         if should_rx_exclude_path(sub_fs_path_in_save, excludes):
             continue
+        assert path_w_meta is not None, '"." not before children in dir'
+        sub_path = path_w_meta + (entry,)
         if not S_ISDIR(vfs.item_mode(sub_item)):
-            yield sub_path
+            yield sub_path, None
         else:
             conv_item, oid, _ = \
                 _previous_conversion(dstrepo, sub_item, True, db, mapping)
+            assert conv_item.oid == sub_item.oid
             if conv_item is not sub_item:
                 sub_path = sub_path[:-1] + ((sub_path[-1][0], conv_item),)
             if oid:
-                yield sub_path
+                yield sub_path, oid
             else:
+                sub_rpath = _replacement_parents + (conv_item.oid,)
                 yield from _vfs_walk_dir_recursively(srcrepo, dstrepo, sub_path,
                                                      excludes, db, mapping,
-                                                     missing)
-    yield path
+                                                     missing,
+                                                     _replacement_parents=sub_rpath)
+    assert path_w_meta is not None, f'{path_msg(fs_path_in_save)} has no "."'
+    assert isinstance(path_w_meta[-1][1].meta, Metadata), path_w_meta
+    yield path_w_meta, None
 
 def _rewrite_link(path, item_mode, srcrepo, dstrepo, stack, missing):
     assert isinstance(missing, MissingConfig), missing
@@ -257,8 +275,8 @@ def _remember_rewrite(from_oid, to_oid, chunked, size, wdbc, mapping):
                      '   values (?, ?, ?, ?)',
                      (from_oid, to_oid, chunked, size))
 
-def _rewrite_save_item(save_path, path, srcrepo, dstrepo, split_cfg, stack,
-                       wdbc, mapping, missing):
+def _rewrite_save_item(save_path, path, replacement_dir, srcrepo, dstrepo,
+                       split_cfg, stack, wdbc, mapping, missing):
     """Returns either None, or, if a directory was missing, the
     directory path components.
 
@@ -272,41 +290,28 @@ def _rewrite_save_item(save_path, path, srcrepo, dstrepo, split_cfg, stack,
         incomplete = path
         path = incomplete.path
 
-    # save_path is the vfs path to the save ref, e.g. to branch/DATE
+    # save_path is the full vfs save path e.g. branch/DATE.
+    fs_path = path[2:] # drop everything before the save
+    assert isinstance(fs_path[0][1], vfs.Commit), fs_path[0]
+    name, item = path[-1]
+    item_mode = vfs.item_mode(item)
+    is_dir = S_ISDIR(item_mode)
+    dir_path = fs_path if is_dir else fs_path[:-1]
 
-    fs_path = _fs_path_from_vfs((path[0],) + path[3:]) # not including /branch/DATE
-    assert fs_path.startswith(b'/'), fs_path
-    fs_path = fs_path[1:] # because resolve(parent=...)
-
-    dirn, filen = os.path.split(b'/' + fs_path)
-    assert dirn.startswith(b'/')
-    dirp = path_components(dirn)
-
-    # If switching to a new sub-tree, finish the current sub-tree.
-    while list(stack.path()) > [x[0] for x in dirp]:
+    # If switching to a new sub-tree, finish the current sub-tree, and
+    # then we'll establish the sub-tree for the new sub-tree via
+    # extend_stack for the missing components.
+    while list(stack.path()) > [x[0] for x in dir_path]:
         stack.pop()
 
-    def push_parents(parents):
-        # FIXME: add missing object support
-        # If switching to a new sub-tree, start a new sub-tree.
-        comp_parent = None
-        for path_component in parents:
-            comp_name, comp_path = path_component
-            if comp_parent:
-                dir_res = vfs.resolve(srcrepo, comp_name, parent=comp_parent)
-            else:
-                full_comp_path = b'/'.join([x[0] for x in save_path]) + comp_path
-                dir_res = vfs.resolve(srcrepo, full_comp_path)
-            meta = dir_res[-1][1].meta
-            if not isinstance(meta, metadata.Metadata):
-                meta = None
-            stack.push(comp_name, meta)
-            comp_parent = dir_res
+    def extend_stack(parents):
+        for parent in parents:
+            stack.push(parent[0], parent[1].meta)
 
-    if incomplete:
+    if incomplete: # must be a dir
+        assert replacement_dir is None, replacement_dir
         assert missing.mode == 'replace', missing
-        # everything except the dir we're replacing
-        push_parents(dirp[:-1][len(stack):])
+        extend_stack(dir_path[len(stack):-1])
         repair_info = missing.repair_info
         # For now, wholesale replacement (no attempt to handle
         # partially readable split trees).
@@ -325,14 +330,9 @@ def _rewrite_save_item(save_path, path, srcrepo, dstrepo, split_cfg, stack,
                                 replacement.oid, replacement.meta)
         return
 
-    push_parents(dirp[len(stack):])
-
-    item = path[-1][1]
-
     # First, things that can't be affected by the rewrite
-    item_mode = vfs.item_mode(item)
     if S_ISLNK(item_mode):
-        assert filen == path[-1][0]
+        extend_stack(dir_path[len(stack):])
         _rewrite_link(path, item_mode, srcrepo, dstrepo, stack, missing)
         return
     if not S_ISREG(item_mode) and not S_ISDIR(item_mode):
@@ -340,35 +340,50 @@ def _rewrite_save_item(save_path, path, srcrepo, dstrepo, split_cfg, stack,
         # described by its metadata, and so bup just saves an empty
         # "placeholder" blob in the git tree (so the tree and .bupm
         # will match up).
+        extend_stack(dir_path[len(stack):])
         git_mode, oid = GIT_MODE_FILE, dstrepo.write_data(b'')
-        stack.append_to_current(filen, item_mode, git_mode, oid, item.meta)
+        stack.append_to_current(name, item_mode, git_mode, oid, item.meta)
         return
 
-    item, oid, git_mode = \
-        _previous_conversion(dstrepo, item, not filen, wdbc, mapping)
-
-    if not filen:
-        # Since there's no filename, this is a subdir -- finish it.
+    if is_dir: # dirs come after their contents, so finish up
+        assert is_dir, path
         assert S_ISDIR(item_mode)
-        assert git_mode is None, item.oid.hex() # for both exists and not
+        if replacement_dir is not None:
+            # This is a directory that we've already converted; don't
+            # push/pop it, just add the previously generated tree to
+            # the parent.
+            extend_stack(dir_path[len(stack):-1]) # establish the parent
+            dir_name, dir_item = dir_path[-1]
+            stack.append_to_current(dir_name, GIT_MODE_TREE, GIT_MODE_TREE,
+                                    replacement_dir, None)
+            return
+        extend_stack(dir_path[len(stack):]) # establish the parent
         if len(stack) == 1:
             return # We're at the top level -- keep the current root dir
-        newtree = stack.pop(override_tree=oid)
+        newtree = stack.pop()
+        assert len(item.oid) == 20, item.oid
+        assert len(newtree) == 20, newtree
         # Don't remember any trees when we're making destructive
         # repairs because walk will skip the contents for a tree that
         # has missing objects when it encounters it a second time (for
         # say the second of two saves during an --append), which will
         # omit the logging, repair trailers, etc.
-        if oid is None and missing.mode != 'replace':
+        if not missing.mode == 'replace':
             wdbc.execute(f'insert into {mapping} (src, dst) values (?, ?)',
                          (item.oid, newtree))
         return
+
+    extend_stack(dir_path[len(stack):])
+
+    item, oid, git_mode = \
+        _previous_conversion(dstrepo, item, is_dir, wdbc, mapping)
+    item_mode = vfs.item_mode(item)
 
     assert S_ISREG(item_mode)
     if oid is not None:
         # already converted - oid and mode are known
         assert git_mode in (GIT_MODE_TREE, GIT_MODE_FILE)
-        stack.append_to_current(filen, item_mode, git_mode, oid, item.meta)
+        stack.append_to_current(name, item_mode, git_mode, oid, item.meta)
         return
 
     item_size = None
@@ -397,7 +412,7 @@ def _rewrite_save_item(save_path, path, srcrepo, dstrepo, split_cfg, stack,
         # contextual argument, and because the type may change from
         # tree to blob.
         assert replacement.meta.mode == default_file_mode, repr(replacement)
-        stack.append_to_current(filen, replacement.meta.mode, GIT_MODE_FILE,
+        stack.append_to_current(name, replacement.meta.mode, GIT_MODE_FILE,
                                 replacement.oid, replacement.meta)
         return
 
@@ -411,7 +426,7 @@ def _rewrite_save_item(save_path, path, srcrepo, dstrepo, split_cfg, stack,
     chunked = 1 if S_ISDIR(git_mode) else 0
 
     _remember_rewrite(item.oid, oid, chunked, item_size, wdbc, mapping)
-    stack.append_to_current(filen, item_mode, git_mode, oid, item.meta)
+    stack.append_to_current(name, item_mode, git_mode, oid, item.meta)
 
 class Rewriter:
     def __init__(self, *, split_cfg, db=None):
@@ -469,11 +484,12 @@ class Rewriter:
                 # and so if a dir is broken, we'll see that "up
                 # front", and never produce any children.
 
-                for path in _vfs_walk_dir_recursively(srcrepo, dstrepo,
-                                                      save_path, excludes,
-                                                      dbc, self._mapping,
-                                                      missing):
-                    _rewrite_save_item(save_path, path, srcrepo, dstrepo,
+                for path, replacement_dir \
+                        in _vfs_walk_dir_recursively(srcrepo, dstrepo, save_path,
+                                                     excludes, dbc, self._mapping,
+                                                     missing):
+                    _rewrite_save_item(save_path, path, replacement_dir,
+                                       srcrepo, dstrepo,
                                        self._split_cfg, stack, dbc,
                                        self._mapping, missing)
 
