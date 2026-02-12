c0c132f487e3482df29a55a06b178cd9fb90cc53
Rob Browning <rlb@defaultvalue.org>
2026-02-12 14:29:35 -0600

    ls: don't require a local repo (e.g. for --remote)
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- lib/bup/cmd/ls.py
+++ lib/bup/cmd/ls.py
@@ -1,12 +1,10 @@
 
 import sys
 
-from bup import git, ls
+from bup import ls
 from bup.io import byte_stream
 
 def main(argv):
-    git.check_repo_or_die()
-
     sys.stdout.flush()
     out = byte_stream(sys.stdout)
     # Check out lib/bup/ls.py for the opt spec
