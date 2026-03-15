0844d0ff09be1c134af2749555c113e382c33552
Rob Browning <rlb@defaultvalue.org>
2026-03-15 15:50:53 -0500

    drecurse: stop changing the working directory (don't chdir)
    Now that all the relevant operations rely on a directory file
    descriptor, stop changing the working directory during the traversal.
    
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- lib/bup/drecurse.py
+++ lib/bup/drecurse.py
@@ -14,11 +14,6 @@ from bup import xstat
 from bup.io import path_msg
 
 
-# the use of fchdir() and lstat() is for two reasons:
-#  - help out the kernel by not making it repeatedly look up the absolute path
-#  - avoid race conditions caused by doing listdir() on a changing symlink
-
-
 def _dirlist(fd):
     l = []
     for n in os.listdir(fd):
@@ -65,7 +60,6 @@ def _recursive_dirlist(prepend, dir_fd, xdev,
             yield path, pst
             continue
         with finalized(sub_fd, os.close):
-            os.fchdir(sub_fd)
             yield from _recursive_dirlist(prepend=path,
                                           dir_fd=sub_fd,
                                           xdev=xdev,
@@ -73,7 +67,6 @@ def _recursive_dirlist(prepend, dir_fd, xdev,
                                           excluded_paths=excluded_paths,
                                           exclude_rxs=exclude_rxs,
                                           xdev_exceptions=xdev_exceptions)
-        os.chdir(b'..')
         yield path, pst
 
 
@@ -83,35 +76,28 @@ def recursive_dirlist(paths, xdev, bup_dir=None,
                       xdev_exceptions=frozenset()):
     for path in paths:
         assert isinstance(path, bytes), path
-    startdir = open_noatime(b'.', O_NOFOLLOW | O_DIRECTORY)
-    with finalized(startdir, os.close):
+    for path in paths:
         try:
-            for path in paths:
-                try:
-                    pst = xstat.lstat(path)
-                except OSError as e:
-                    add_error('recursive_dirlist: %s' % e)
-                    continue
-                if not stat.S_ISDIR(pst.st_mode):
-                    yield path, pst
-                    continue
-                try:
-                    path_fd = open_noatime(path, O_NOFOLLOW | O_DIRECTORY)
-                except OSError as e:
-                    add_error(e)
-                    continue
-                with finalized(path_fd, os.close):
-                    os.fchdir(path_fd)
-                    xdev = pst.st_dev if xdev else None
-                    prepend = path if path[-1] == b'/'[0] else path + b'/'
-                    yield from _recursive_dirlist(prepend=prepend,
-                                                  dir_fd=path_fd,
-                                                  xdev=xdev,
-                                                  bup_dir=bup_dir,
-                                                  excluded_paths=excluded_paths,
-                                                  exclude_rxs=exclude_rxs,
-                                                  xdev_exceptions=xdev_exceptions)
-                    os.fchdir(startdir)
-                yield (prepend,pst)
-        finally:
-            os.fchdir(startdir)
+            pst = xstat.lstat(path)
+        except OSError as e:
+            add_error('recursive_dirlist: %s' % e)
+            continue
+        if not stat.S_ISDIR(pst.st_mode):
+            yield path, pst
+            continue
+        try:
+            path_fd = open_noatime(path, O_NOFOLLOW | O_DIRECTORY)
+        except OSError as e:
+            add_error(e)
+            continue
+        with finalized(path_fd, os.close):
+            xdev = pst.st_dev if xdev else None
+            prepend = path if path[-1] == b'/'[0] else path + b'/'
+            yield from _recursive_dirlist(prepend=prepend,
+                                          dir_fd=path_fd,
+                                          xdev=xdev,
+                                          bup_dir=bup_dir,
+                                          excluded_paths=excluded_paths,
+                                          exclude_rxs=exclude_rxs,
+                                          xdev_exceptions=xdev_exceptions)
+        yield prepend, pst
