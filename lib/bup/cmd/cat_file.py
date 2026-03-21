74260ef112b4ca7f2e7c787092c14c86efbf6e08
Rob Browning <rlb@defaultvalue.org>
2026-03-20 21:34:53 -0500

    test-cat-file: test more invalid invocations
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- lib/bup/cmd/cat_file.py
+++ lib/bup/cmd/cat_file.py
@@ -5,7 +5,7 @@ import re, stat, sys
 from bup import options, git, vfs
 from bup.compat import argv_bytes
 from bup.helpers import EXIT_FAILURE, chunkyreader, log
-from bup.io import byte_stream
+from bup.io import byte_stream, path_msg as pm
 from bup.repo import LocalRepo
 
 optspec = """
@@ -31,14 +31,14 @@ def main(argv):
     target = argv_bytes(extra[0])
 
     if not re.match(br'/*[^/]+/[^/]+', target):
-        o.fatal("path %r doesn't include a branch and revision" % target)
+        o.fatal(f"path {pm(target)} doesn't include a branch and revision")
 
     with LocalRepo() as repo:
         resolved = vfs.resolve(repo, target, follow=False)
         leaf_item = resolved[-1][1]
         if not leaf_item:
-            log('error: cannot access %r in %r\n'
-                % (b'/'.join(name for name, item in resolved), target))
+            res_path = b'/'.join(name for name, item in resolved)
+            log(f'error: cannot access {pm(res_path)} in {pm(target)}\n')
             sys.exit(EXIT_FAILURE)
 
         mode = vfs.item_mode(leaf_item)
@@ -48,7 +48,7 @@ def main(argv):
 
         if opt.bupm:
             if not stat.S_ISDIR(mode):
-                o.fatal('%r is not a directory' % target)
+                o.fatal(f'{pm(target)} is not a directory')
             _, bupm_oid = vfs.tree_data_and_bupm(repo, leaf_item.oid)
             if bupm_oid:
                 with vfs.tree_data_reader(repo, bupm_oid) as meta_stream:
@@ -62,4 +62,4 @@ def main(argv):
                     for b in chunkyreader(f):
                         out.write(b)
             else:
-                o.fatal('%r is not a plain file' % target)
+                o.fatal(f'{pm(target)} is not a plain file')
