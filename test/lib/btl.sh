1e8e3118c54b2dfbffa15e49f56f17feccce9e43
Rob Browning <rlb@defaultvalue.org>
2026-03-29 11:07:29 -0500

    Capture test output via out-to err-to both-to instead of eval/tee
    The
    
      WVPASS eval 'cmd ... 2> >(tee log)'
      WVPASS grep ... log
    
    approach to capture and examine cmd's stderr is racy because it
    doesn't wait for the tee to finish.  We can, and have in some recent
    cases like
    
      61dff81a99eac59d00f5ce1a26e6e1d1ee11c8dd
      get: improve symlink repairs and add tests
    
    avoided the race via an extra fifo like so:
    
      WVPASS mkfifo done
      ...
      WVPASS eval 'cmd ... 2> >(tee log; : > done)'
      WFVAIL read done
      WVPASS grep ... log
    
    Instead of distributing that approach everywhere, add new btl.sh
    out-to, err-to, and both-to commands that can handle the same task
    like this:
    
      WVPASS err-to log cmd ...
      WVPASS grep ... log
    
    which is simpler, both in appearance and internally, since it just
    uses some extra plumbing (file descriptors) to do the same job.
    
    Replace all the existing eval/tee/fifo uses with equivalent *-to
    invocations.
    
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- test/lib/btl.sh
+++ test/lib/btl.sh
@@ -24,3 +24,47 @@ btl-display-file()
     cat "$name" || exit $?
     printf -- "----- \"%q\" content above -----\n" "$name" || exit $?
 }
+
+
+# out-to, err-to, and both-to allow redirections of just stdout or
+# stderr when using "wrapper" commands like WVPASS, WVFAIL, ...  For
+# example
+#
+#     WVPASS err-to err.log some --random --comand
+#     WVPASS grep ... err.log
+#
+# This avoids capturing the WV command's output in the log which
+# happens for something like "WVPASS ... > err.log".  This also makes
+# sure the comand doesn't return until the output is finished, which
+# isn't the case for a process substitution like this:
+#
+#     WVPASS eval "some --command 2> >(tee err.log)"
+#
+
+out-to()
+{
+    test $# -gt 1 || { echo 'Usage: out-to OUT cmd ...' 1>&2; exit 2; }
+    local out="$1"
+    shift
+    "$@" | tee "$out"
+    return "${PIPESTATUS[0]}"
+}
+
+err-to()
+{
+    test $# -gt 1 || { echo 'Usage: err-to ERR cmd ...' 1>&2; exit 2; }
+    local err="$1" fd rc
+    shift
+    { "$@" 2>&1 >&${fd} {fd}>&- | tee "$err" {fd}>&-; rc="${PIPESTATUS[0]}"; } \
+        {fd}>&1
+    : {fd}>&-
+    return $rc
+}
+
+both-to()
+{
+    test $# -gt 2 || { echo 'Usage: both-to OUT ERR cmd ...' 1>&2; exit 2; }
+    local out="$1" err="$2"
+    shift 2
+    err-to "$err" out-to "$out" "$@"
+}
